7d851359fbd334476d74916123713c709652ea43
Mario Garrido <login@mariogarrido.pt>
2023-11-01 13:52:18 +0000

    feat: create notifications table on databse

---
+++ api/migrations/20231101120631_create_notifications_table.sql
@@ -0,0 +1,16 @@
+-- migrate:up
+CREATE TABLE `notifications` (
+    `id` int AUTO_INCREMENT,
+    `title` varchar(60) NOT NULL UNIQUE,
+    `message` text NOT NULL,
+    `ack` tinyint(1) NOT NULL DEFAULT 0,
+    `created_at` datetime NOT NULL DEFAULT NOW(),
+    `target_user_id` INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (target_user_id) REFERENCES auth(id)
+);
+
+-- migrate:down
+SET FOREIGN_KEY_CHECKS=0; -- to disable them
+DROP TABLE IF EXISTS `notifications`;
+SET FOREIGN_KEY_CHECKS=1; -- to re-enable them
